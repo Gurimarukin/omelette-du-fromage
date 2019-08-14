59f773eec787e95af00f585818d6c239649218cc
Gurimarukin <naxel@laposte.net>
2019-08-14 09:51:32 +0200

    clean comments, add rate_limiter dep to scrapers

--- apps/rate_limiter/lib/rate_limiter/application.ex
+++ apps/rate_limiter/lib/rate_limiter/application.ex
@@ -1,19 +1,13 @@
 defmodule RateLimiter.Application do
-  # See https://hexdocs.pm/elixir/Application.html
-  # for more information on OTP Applications
   @moduledoc false
 
   use Application
 
   def start(_type, _args) do
     children = [
-      # Starts a worker by calling: RateLimiter.Worker.start_link(arg)
-      # {RateLimiter.Worker, arg}
       {DynamicSupervisor, name: RateLimiter.LimitersSupervisor, strategy: :one_for_one}
     ]
 
-    # See https://hexdocs.pm/elixir/Supervisor.html
-    # for other strategies and supported options
     opts = [strategy: :one_for_one, name: RateLimiter.Supervisor]
     Supervisor.start_link(children, opts)
   end
--- apps/shows_store/lib/shows_store/application.ex
+++ apps/shows_store/lib/shows_store/application.ex
@@ -1,19 +1,13 @@
 defmodule ShowsStore.Application do
-  # See https://hexdocs.pm/elixir/Application.html
-  # for more information on OTP Applications
   @moduledoc false
 
   use Application
 
   def start(_type, _args) do
     children = [
-      # Starts a worker by calling: ShowsStore.Worker.start_link(arg)
-      # {ShowsStore.Worker, arg}
       ShowsStore.Repo
     ]
 
-    # See https://hexdocs.pm/elixir/Supervisor.html
-    # for other strategies and supported options
     opts = [strategy: :one_for_one, name: ShowsStore.Supervisor]
     Supervisor.start_link(children, opts)
   end
