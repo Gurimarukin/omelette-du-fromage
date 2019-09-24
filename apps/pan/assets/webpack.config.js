36faaa00e027408e94dd1a668975f1c7c4812965
Gurimarukin <naxel@laposte.net>
2019-09-24 22:55:53 +0200

    add fonts

--- apps/pan/assets/webpack.config.js
+++ apps/pan/assets/webpack.config.js
@@ -6,36 +6,48 @@ const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = (env, options) => ({
-  optimization: {
-    minimizer: [
-      new UglifyJsPlugin({ cache: true, parallel: true, sourceMap: false }),
-      new OptimizeCSSAssetsPlugin({})
+    optimization: {
+        minimizer: [
+            new UglifyJsPlugin({
+                cache: true,
+                parallel: true,
+                sourceMap: false
+            }),
+            new OptimizeCSSAssetsPlugin({})
+        ]
+    },
+    entry: {
+        './js/app.js': glob.sync('./vendor/**/*.js').concat(['./js/app.js'])
+    },
+    output: {
+        filename: 'app.js',
+        path: path.resolve(__dirname, '../priv/static/js')
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader'
+                }
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            url: false
+                        }
+                    }
+                ]
+            }
+        ]
+    },
+    plugins: [
+        new MiniCssExtractPlugin({ filename: '../css/app.css' }),
+        new CopyWebpackPlugin([{ from: 'static/', to: '../' }])
     ]
-  },
-  entry: {
-    './js/app.js': glob.sync('./vendor/**/*.js').concat(['./js/app.js'])
-  },
-  output: {
-    filename: 'app.js',
-    path: path.resolve(__dirname, '../priv/static/js')
-  },
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader'
-        }
-      },
-      {
-        test: /\.css$/,
-        use: [MiniCssExtractPlugin.loader, 'css-loader']
-      }
-    ]
-  },
-  plugins: [
-    new MiniCssExtractPlugin({ filename: '../css/app.css' }),
-    new CopyWebpackPlugin([{ from: 'static/', to: '../' }])
-  ]
 });
--- apps/pan/lib/pan_web/views/layout_view.ex
+++ apps/pan/lib/pan_web/views/layout_view.ex
@@ -1,3 +1,14 @@
 defmodule PanWeb.LayoutView do
   use PanWeb, :view
+
+  def cat_link(conn, label, to: to) do
+    class =
+      if Phoenix.Controller.current_path(conn, %{}) === to do
+        "root__link_current"
+      else
+        "root__link"
+      end
+
+    link(label, to: to, class: class)
+  end
 end
